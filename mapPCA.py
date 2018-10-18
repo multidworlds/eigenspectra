733dc621406541396f262ed492c15a06fd5ac688
jacob <jarcangeli92@gmail.com>
2018-10-18 14:31:25 -0400

    Added PCA test

--- mapPCA.py
+++ mapPCA.py
@@ -2,13 +2,15 @@
  '''
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+import numpy as np
+
 def getEigenSpec(X,n_comp):
 	#Returns the Eigen Spectra for the dataset
 
 	#Expects flux maps in the form (lam,lat,long)
-	lamdim=fluxmap.shape[0]
-	latdim=fluxmap.shape[1]
-	longdim=fluxmap.shape[2]
+	lamdim=X.shape[0]
+	latdim=X.shape[1]
+	longdim=X.shape[2]
 	#Flatten latitude and longitude to get 2D array, with second element a spectrum at each location
 	Xreshaped=X.transpose(1,2,0).reshape(-1,X.transpose(1,2,0).shape[2])
 
@@ -27,9 +29,9 @@ def getPCArepresentation(X,n_comp):
 	#Returns map projected in lower dimensional spectral space using Eigen Spectra
 
 	#Expects flux maps in the form (lam,lat,long)
-	lamdim=fluxmap.shape[0]
-	latdim=fluxmap.shape[1]
-	longdim=fluxmap.shape[2]
+	lamdim=X.shape[0]
+	latdim=X.shape[1]
+	longdim=X.shape[2]
 	#Flatten latitude and longitude to get 2D array, with second element a spectrum at each location
 	Xreshaped=X.transpose(1,2,0).reshape(-1,X.transpose(1,2,0).shape[2])
 
@@ -43,5 +45,5 @@ def getPCArepresentation(X,n_comp):
 	modelPCA_unscaled = np.dot(principalComponents,pca.components_)
 	fullModel = scaler.inverse_transform(modelPCA_unscaled)
 	#Reshape model to Spectrum(lam,lat,long)
-	return fullModel.transpose(1,0).reshape(10,16,18)
+	return fullModel.transpose(1,0).reshape(lamdim,latdim,longdim)
 

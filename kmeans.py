1cd0a252a66248d22ae688fe0db139631874cbaf
prashansa <prashansa17793@gmail.com>
2018-10-18 16:37:58 -0400

    loop over different k values

--- kmeans.py
+++ kmeans.py
@@ -4,6 +4,24 @@
 
 from sklearn.cluster import KMeans
 
+import numpy as np
+import scipy
+
+import sklearn
+import matplotlib.pyplot as plt
+
+
+from sklearn import cluster, datasets, decomposition
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+
+from sklearn import metrics
+from sklearn.metrics import pairwise_distances
+from sklearn import datasets
+
+
 def kmeans(fluxmap,k,labels=False):
 	#Expects flux maps in the form (lam,lat,long)
 	lamdim=fluxmap.shape[0]
@@ -12,3 +30,17 @@ def kmeans(fluxmap,k,labels=False):
 	kmeans = KMeans(n_clusters=k, random_state=0).fit(fluxmap.transpose(1,2,0).reshape(-1,
 		fluxmap.transpose(1,2,0).shape[2]))
 	return (kmeans.labels_).reshape(latdim,longdim)
+
+
+
+
+for i in range(20): #try for k values from 0 to 20
+    
+    
+    labels.append(kmeans(fluxmap,i))
+    score.append(metrics.silhouette_score(fluxMap.reshape(), labelsForThisOne, metric='euclidean'))
+
+
+
+
+

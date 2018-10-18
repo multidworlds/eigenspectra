ade24c0c5fc15f3375fff586891be5d2933d5821
Dylan Keating <dylan.keating@mail.mcgill.ca>
2018-10-18 13:27:45 -0400

    Add files via upload

---
+++ mapPCA.py
@@ -0,0 +1,47 @@
+'''' Routines to get Eigenspectra, and lower dimensional projection, given a map in the form Spec(lam,lat,long)
+ '''
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+def getEigenSpec(X,n_comp):
+	#Returns the Eigen Spectra for the dataset
+
+	#Expects flux maps in the form (lam,lat,long)
+	lamdim=fluxmap.shape[0]
+	latdim=fluxmap.shape[1]
+	longdim=fluxmap.shape[2]
+	#Flatten latitude and longitude to get 2D array, with second element a spectrum at each location
+	Xreshaped=X.transpose(1,2,0).reshape(-1,X.transpose(1,2,0).shape[2])
+
+	#Scaler to first remove mean, because PCA destroys information about mean
+	scaler = StandardScaler(with_std=False,with_mean=True)
+	x = scaler.fit_transform(Xreshaped)
+	#Do PCA
+	pca = PCA(n_components=n_comp)
+	#Get coefficients. pca.components_ gives Eigenvectors.
+	principalComponents = pca.fit_transform(Xreshaped)
+	modelPCA_unscaled = np.dot(principalComponents,pca.components_)
+	#Return eigenspectra
+	return pca.components_
+
+def getPCArepresentation(X,n_comp):
+	#Returns map projected in lower dimensional spectral space using Eigen Spectra
+
+	#Expects flux maps in the form (lam,lat,long)
+	lamdim=fluxmap.shape[0]
+	latdim=fluxmap.shape[1]
+	longdim=fluxmap.shape[2]
+	#Flatten latitude and longitude to get 2D array, with second element a spectrum at each location
+	Xreshaped=X.transpose(1,2,0).reshape(-1,X.transpose(1,2,0).shape[2])
+
+	#Scaler to first remove mean, because PCA destroys information about mean
+	scaler = StandardScaler(with_std=False,with_mean=True)
+	x = scaler.fit_transform(Xreshaped)
+	#Do PCA
+	pca = PCA(n_components=n_comp)
+	#Get coefficients. pca.components_ gives Eigenvectors.
+	principalComponents = pca.fit_transform(Xreshaped)
+	modelPCA_unscaled = np.dot(principalComponents,pca.components_)
+	fullModel = scaler.inverse_transform(modelPCA_unscaled)
+	#Reshape model to Spectrum(lam,lat,long)
+	return fullModel.transpose(1,0).reshape(10,16,18)
+

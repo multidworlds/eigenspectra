cc9d8390e7d9a4953ff9b953429753f406dfe365
jacob <jarcangeli92@gmail.com>
2018-10-19 17:54:33 -0400

    PEP8 Compliant

--- kmeans.py
+++ kmeans.py
@@ -10,11 +10,10 @@ from sklearn.cluster import KMeans
 from sklearn import metrics
 
 
-def kmeans(fluxmap,k,labels=False):
+def kmeans(fluxmap, k, labels=False):
     '''
     Compute k-means clustering of spectra for a given number of groups.
 
-   
     Parameters
     ----------
     fluxmap : array of Fp/Fs (axes: wavelength x latitude x longitude)
@@ -24,16 +23,16 @@ def kmeans(fluxmap,k,labels=False):
     -------
     kgroups: array of group indices (ints) from 0 to k-1 (axes: lat x lon)
     '''
-	#Expects flux maps in the form (lam,lat,long)
-    lamdim=fluxmap.shape[0]
-    latdim=fluxmap.shape[1]
-    longdim=fluxmap.shape[2]
-    kmeans = KMeans(n_clusters=k, random_state=0).fit(fluxmap.transpose(1,2,0).reshape(-1,
-		fluxmap.transpose(1,2,0).shape[2]))
-    kgroups = (kmeans.labels_).reshape(latdim,longdim)
+
+	# Expects flux maps in the form (lam,lat,long)
+
+    lamdim = fluxmap.shape[0]
+    latdim = fluxmap.shape[1]
+    longdim = fluxmap.shape[2]
+    _fluxmap = fluxmap.transpose(1, 2, 0).reshape(-1, fluxmap.shape[0])
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(_fluxmap)
+    kgroups = (kmeans.labels_).reshape(latdim, longdim)
     return kgroups
-
-
 
 
 def kmeansBest(fluxmap, n=10):
@@ -51,13 +50,13 @@ def kmeansBest(fluxmap, n=10):
     '''
 
     labels, score = np.zeros(n), np.zeros(n)
-    
-    for i in range(n): #try for k values from 0 to n
-        label = kmeans(fluxmap,i)
+
+    for i in range(n):  # try for k values from 0 to n
+        label = kmeans(fluxmap, i)
         labels[i] = label
         score[i] = metrics.silhouette_score(fluxmap, label, metric='euclidean')
-    
-    #choose the highest score!
+
+    # choose the highest score!
     k = np.argmax(score)
     return k
 

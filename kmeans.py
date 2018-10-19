bb1bea3d1cd94916499a921127a774c590a422b5
prashansa <prashansa17793@gmail.com>
2018-10-19 11:01:01 -0400

    best k means

--- kmeans.py
+++ kmeans.py
@@ -33,7 +33,9 @@ def kmeans(fluxmap,k,labels=False):
 
 
 
+'''
 n = 20
+<<<<<<< HEAD
 def kmeansBest(n):
     
     labels, score = np.zeros(n), np.zeros(n)
@@ -48,5 +50,18 @@ def kmeansBest(n):
     k = np.where(score==np.max(score))
     labels = np.where(score==np.max(score))
 
+=======
+labels, score = np.zeros(n), np.zeros(n)
+
+for i in range(n): #try for k values from 0 to 20
+
+    labels[i] = kmeans(fluxmap,i)
+    score[i] = metrics.silhouette_score(fluxMap.reshape(), labelsForThisOne, metric='euclidean')
+
+#choose the highest score!
+k = np.where(score==np.max(score))
+labels = np.where(score==np.max(score))
+'''
+>>>>>>> 1fa036662d2a4f20835f7b49b3e70c0b1c0ec514
 
 

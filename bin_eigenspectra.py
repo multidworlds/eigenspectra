84eb82c8b2cbd070733b1cc2a806546e9b48d618
jacob <jarcangeli92@gmail.com>
2018-10-19 17:45:44 -0400

    Made PEP8 compliant

--- bin_eigenspectra.py
+++ bin_eigenspectra.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+
 def bin_eigenspectra(spectra, kgroups):
     '''
     Converts a grid of spectra into eigenspectra defined by kgroups.
-    
+
     Parameters
     ----------
     spectra : array of Fp/Fs (axes: wavelengths x lat x lon)
@@ -17,17 +18,17 @@ def bin_eigenspectra(spectra, kgroups):
     # Calculate the number of groups
     # assuming kgroups contains integers from 0 to ngroups
     ngroups = np.max(kgroups)+1
-    
-    nbins = spectra.shape[0] # number of wavelength bins
-    # Flatten over (lat x lon)
-    spectra = spectra.reshape(nbins,-1)
+
+    nbins = spectra.shape[0]  # number of wavelength bins
+    spectra = spectra.reshape(nbins, -1)  # Flatten over (lat x lon)
     kgroups = kgroups.reshape(-1)
 
     eigenspectra = []
     for g in range(ngroups):
         ingroup = (kgroups == g).astype(int)
-        eigenspec = np.sum(spectra*ingroup, axis=1)/np.sum(ingroup) 
+        eigenspec = np.sum(spectra*ingroup, axis=1)/np.sum(ingroup)
         # eigenspec is the mean of spectra in group
         eigenspectra.append(eigenspec)
-    
+
     return eigenspectra
+

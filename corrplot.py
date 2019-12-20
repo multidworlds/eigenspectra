5bf320c81ce63e310f0f1ff8aa53c059b492e960
Megan Mansfield <meganmansfield@uchicago.edu>
2019-12-20 10:24:41 -0600

    Corrplot.py calculates correlation coefficients and corresponding p values so you can check whether a correlation is significant.

--- corrplot.py
+++ corrplot.py
@@ -1,13 +1,16 @@
 #Make crosscorrelation plot showing how correlated the eigencurve coefficients are.
 
 import numpy as np
+from scipy import stats
 
 def corrplot(file,waveindex):
 	tmp=np.load(file)
 	outDictionary = tmp['arr_0'].tolist()
 	eigen=outDictionary['eigencurve coefficients'][waveindex]
 	corrcoeffs=-np.ones((np.shape(eigen)[1],np.shape(eigen)[1]))
+	pearsonr=-np.ones((np.shape(eigen)[1],np.shape(eigen)[1]))
 	for i in np.arange(np.shape(eigen)[1]):
 		for j in np.arange(i+1,np.shape(eigen)[1]):
-			corrcoeffs[j,i]=np.corrcoef(eigen[:,i],eigen[:,j])[0,1]
-	return corrcoeffs
+			corrcoeffs[j,i]=stats.pearsonr(eigen[:,i],eigen[:,j])[0]
+			pearsonr[j,i]=stats.pearsonr(eigen[:,i],eigen[:,j])[1]
+	return corrcoeffs, pearsonr
